4dea048f3e111dfa0b17d7ae3f3257d07d33eb18
Gerardo Castillo <gerrcass@gmail.com>
2019-09-22 13:17:42 -0400

    adding ShoppingIcon component and putting it in the header

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -5,8 +5,10 @@ import {connect} from 'react-redux'
 import { ReactComponent as Logo} from '../../assets/crown.svg'
 
 import { auth } from '../../firebase/firebase.utils'
+import CartIcon from '../cart-icon/cart-icon.component'
 
 import './header.styles.scss'
+
 
 const Header = ({currentUserRedux}) => (
     <div className='header'>
@@ -22,6 +24,7 @@ const Header = ({currentUserRedux}) => (
                 :
                     <Link className='option' to='/signin'>SIGN IN</Link>
             }
+            <CartIcon />
         </div>
 
     </div>
