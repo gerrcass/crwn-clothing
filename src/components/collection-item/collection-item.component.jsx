311bf5c9f38eb0da76ef8b2fd4e7f5a6c41214c9
Gerardo Castillo <gerrcass@gmail.com>
2019-09-23 16:18:33 -0400

    refactoring the code for the addItem dispatcher in CollectionItem and CollectionPreview component, small changes to CustomButton component for the inverted color when called from CollectionItem and finally implemented addItemToCard utility function in cartReducer to group item by quantity into carItems redux state

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,21 +1,33 @@
 import React from 'react'
+import {connect} from 'react-redux'
+
+import CustomButton from '../custom-button/custom-button.component'
+import { addItem } from '../../redux/cart/cart.actions'
+
 import './collection-item.styles.scss'
 
-const CollectionItem = ({id,name,imageUrl,price}) => (
-    /* key={id} in this div above? */
-    <div className='collection-item'>
-        <div
-            className='image'
-            style={{
-                backgroundImage: `url(${imageUrl})`
-            }}
-        />
-        <div className='collection-footer'>
-            <span className='name'>{name}</span>
-            <span className='price'>{price}</span>
+const CollectionItem = ({item,addItem}) => {
+    const {name,imageUrl,price} = item
+    return (
+        <div className='collection-item'>
+            <div
+                className='image'
+                style={{
+                    backgroundImage: `url(${imageUrl})`
+                }}
+            />
+            <div className='collection-footer'>
+                <span className='name'>{name}</span>
+                <span className='price'>{price}</span>
+            </div>
+            <CustomButton inverted onClick={() => addItem(item)}>
+                Add to Cart
+            </CustomButton>
+
         </div>
+)}
+const mapDispatchToProps = dispatch => ({
+    addItem: item => dispatch(addItem(item))
 
-    </div>
-)
-
-export default CollectionItem
+})
+export default connect(null,mapDispatchToProps)(CollectionItem)
